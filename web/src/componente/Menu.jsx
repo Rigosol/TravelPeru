72373ab58462c324451b04f559ac43e0aaac35c8
Rigosol <ribersoll@gmail.com>
2024-08-22 23:51:52 +0000

    Se ha cambiado el compoente encabezado en menu

---
+++ web/src/componente/Menu.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../App.css';
+
+function Menu() {
+  return (
+    <header>
+      <div className="logo">
+        <img src="/path-to-your-logo.png" alt="Peru Travel" /> {/* AsegÃšrate de colocar la ruta correcta de la imagen */}
+        <h1 style={{ color: '#A020F0' }}>Todo <span style={{ color: '#FF8000' }}>Turismo</span></h1>
+      </div>
+      <div className="menu-container">
+        <a href="#" className="menu-item">Inicio</a>
+        <a href="#" className="menu-item">Salidas diarias</a>
+        <a href="#" className="menu-item">Full day</a>
+        <a href="#" className="menu-item">Promos</a>
+        <a href="#" className="menu-item">Contacto</a>
+      </div>
+      <div className="search-bar">
+      t  <input type="text" placeholder="Buscar" />
+      </div>
+    </header>
+  );
+}
+
+export default Menu;
